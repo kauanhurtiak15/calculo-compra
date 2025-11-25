a3071a6fbaf2ce2e441280c8dd7d2d67de9bb52f
kauanhurtiak15 <kauan.hurtiak.santos@escola.pr.gov.br>
2025-11-25 12:16:33 +0000

    em teste

---
+++ calculoCompra.py
@@ -0,0 +1,7 @@
+preco_unitario = input("Digite o preco: ")
+quantidade = input("Digite a quantidade: ")
+
+def calcular_compra(total):
+    total = 0
+    total = preco_unitario * quantidade
+    return total
