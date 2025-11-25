79cb97ecc5bad478de7bc9e78bf13c57fcc0d7ac
kauanhurtiak15 <kauan.hurtiak.santos@escola.pr.gov.br>
2025-11-25 14:01:39 +0000

    concluido

--- calculoCompra.py
+++ calculoCompra.py
@@ -1,7 +1,11 @@
-preco_unitario = input("Digite o preco: ")
-quantidade = input("Digite a quantidade: ")
+total = 0
+while True:
+    try:
+        preco_item = float(input("Digite o preço do item (Digite 0 para finalizar o calculo): "))
+        if preco_item == 0:
+            break
+        total += preco_item
+    except ValueError:
+        print("digite um número: ")
 
-def calcular_compra(total):
-    total = 0
-    total = preco_unitario * quantidade
-    return total
+print(f"O total da compra é: R$ {total:.2f}")
